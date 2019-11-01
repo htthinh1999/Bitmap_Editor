573772f47a993df158c9c5118b5a4e92543a7ac5
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2019-11-01 13:57:30 +0700

    GrayScale

--- BitmapEditor_Nhom_8/BitmapEditor.Designer.cs
+++ BitmapEditor_Nhom_8/BitmapEditor.Designer.cs
@@ -31,6 +31,12 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnTurnLeft = new System.Windows.Forms.Button();
+            this.btnTurnRight = new System.Windows.Forms.Button();
+            this.btnOriginal = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,19 +60,86 @@
             // 
             // btnOpen
             // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpen.Location = new System.Drawing.Point(12, 12);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(75, 75);
             this.btnOpen.TabIndex = 1;
-            this.btnOpen.Text = "Open Image";
+            this.btnOpen.Text = "Open\r\nImage";
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(12, 171);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(75, 23);
+            this.btnGray.TabIndex = 2;
+            this.btnGray.Text = "Gray Scale";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(12, 200);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomIn.TabIndex = 3;
+            this.btnZoomIn.Text = "Zoom (+)";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(12, 229);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomOut.TabIndex = 4;
+            this.btnZoomOut.Text = "Zoom (-)";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // btnTurnLeft
+            // 
+            this.btnTurnLeft.Location = new System.Drawing.Point(12, 259);
+            this.btnTurnLeft.Name = "btnTurnLeft";
+            this.btnTurnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnTurnLeft.TabIndex = 5;
+            this.btnTurnLeft.Text = "Turn Left";
+            this.btnTurnLeft.UseVisualStyleBackColor = true;
+            this.btnTurnLeft.Click += new System.EventHandler(this.btnTurnLeft_Click);
+            // 
+            // btnTurnRight
+            // 
+            this.btnTurnRight.Location = new System.Drawing.Point(12, 289);
+            this.btnTurnRight.Name = "btnTurnRight";
+            this.btnTurnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnTurnRight.TabIndex = 6;
+            this.btnTurnRight.Text = "Turn Right";
+            this.btnTurnRight.UseVisualStyleBackColor = true;
+            this.btnTurnRight.Click += new System.EventHandler(this.btnTurnRight_Click);
+            // 
+            // btnOriginal
+            // 
+            this.btnOriginal.Location = new System.Drawing.Point(12, 142);
+            this.btnOriginal.Name = "btnOriginal";
+            this.btnOriginal.Size = new System.Drawing.Size(75, 23);
+            this.btnOriginal.TabIndex = 7;
+            this.btnOriginal.Text = "Original";
+            this.btnOriginal.UseVisualStyleBackColor = true;
+            this.btnOriginal.Click += new System.EventHandler(this.btnOriginal_Click);
             // 
             // BitmapEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(423, 324);
+            this.Controls.Add(this.btnOriginal);
+            this.Controls.Add(this.btnTurnRight);
+            this.Controls.Add(this.btnTurnLeft);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.btnGray);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.pictureBox1);
             this.Name = "BitmapEditor";
@@ -82,6 +155,12 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnTurnLeft;
+        private System.Windows.Forms.Button btnTurnRight;
+        private System.Windows.Forms.Button btnOriginal;
     }
 }
 
--- BitmapEditor_Nhom_8/BitmapEditor.cs
+++ BitmapEditor_Nhom_8/BitmapEditor.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace BitmapEditor_Nhom_8
 {
@@ -23,7 +24,7 @@ namespace BitmapEditor_Nhom_8
         }
 
         private String imagePath;
-        private Bitmap bm;
+        private Bitmap bm=null;
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
@@ -47,6 +48,101 @@ namespace BitmapEditor_Nhom_8
                 imagePath = openFileDialog1.FileName;
                 bm = new Bitmap(imagePath);
                 pictureBox1.Image = bm;
+            }
+        }
+
+        private void btnOriginal_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+                bm = new Bitmap(imagePath);
+                pictureBox1.Image = bm;
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private BitmapData bmData = null;
+        unsafe
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+                bmData = bm.LockBits(new Rectangle(0, 0, bm.Width, bm.Height), ImageLockMode.ReadWrite, bm.PixelFormat);
+                int offset = bmData.Stride - bm.Width * 3;
+                byte* p = (byte*)bmData.Scan0;
+
+                for (int i = 0; i < bm.Width; i++)
+                {
+                    for(int j=0; j<bm.Height; j++)
+                    {
+                        int t = (p[0] + p[1] + p[2]) / 3; //p[0] is blue, p[1] is green, p[2] is red
+                        p[0] = (byte)t;
+                        p[1] = (byte)t;
+                        p[2] = (byte)t;
+
+                        p += 3;
+                    }
+                    p += offset;
+                }
+
+                // Unlock bits
+                bm.UnlockBits(bmData);
+                pictureBox1.Image = bm;
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnTurnLeft_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnTurnRight_Click(object sender, EventArgs e)
+        {
+            if (bm != null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("You must open an image first!!", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
