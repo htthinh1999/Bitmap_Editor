1cda2c1ec8a698fb138eadb1e2b0c6018d0b41cc
Huỳnh Tấn Thịnh <47586956+htthinh1999@users.noreply.github.com>
2019-11-01 12:38:58 +0700

    Initial commit

---
+++ BitmapEditor_Nhom_8/BitmapEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapEditor_Nhom_8
+{
+    public partial class BitmapEditor : Form
+    {
+        public BitmapEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void BitmapEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
